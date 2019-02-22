5c3f720c0aec4f3ef0d5e20c6478f0199e935b74
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-02-22 02:03:26 +0100

    updated endtime

--- configs/generate_configs.sh
+++ configs/generate_configs.sh
@@ -6,6 +6,12 @@ do
     cp -r template nx_${nx};
     cp -r template_large nx_large_${nx};
     sed -i "s/NX/${nx}/g" nx_${nx}/kelvinhelmholtz_3d_tube/kelvinhelmholtz.xml;
+    if [ "$nx" -lt "256" ];
+    then
+       sed -i "s/ENDTIME/0.02/g" nx_${nx}/kelvinhelmholtz_3d_tube/kelvinhelmholtz.xml;
+    else
+	sed -i "s/ENDTIME/0.001/g" nx_${nx}/kelvinhelmholtz_3d_tube/kelvinhelmholtz.xml;
+    fi
     sed -i "s/NX/${nx}/g" nx_large_${nx}/kelvinhelmholtz_3d_tube/kelvinhelmholtz.xml;
     sed -i "s/NX/${nx}/g" nx_large_${nx}/submit.sh;
     sed -i "s/NX/${nx}/g" nx_${nx}/submit.sh;
